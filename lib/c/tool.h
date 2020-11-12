832f900f51e124afa909a44ee53116f3cac4a0a2
Dietrich Epp <depp@zdome.net>
2020-11-12 06:17:12 -0500

    Get VADPCM encoder and decoder working
    GitOrigin-RevId: c85c13196320306743af8944d73c983367d8d382

--- lib/c/tool.h
+++ lib/c/tool.h
@@ -3,6 +3,7 @@
 #include "base/defs.h"
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdnoreturn.h>
 
 // Print an error message and exit.
@@ -14,3 +15,13 @@ noreturn void die_errno(int err, const char *fmt, ...)
 
 // Malloc, but exit the program on failure.
 void *xmalloc(size_t size) __attribute__((malloc));
+
+inline uint16_t swap16(uint16_t x) {
+    return __builtin_bswap16(x);
+}
+
+inline uint32_t swap32(uint32_t x) {
+    return __builtin_bswap32(x);
+}
+
+void swap16arr(int16_t *arr, size_t n);
