59646021c0f4ecafaa9147588b4d62ec198db6b3
Dietrich Epp <depp@zdome.net>
2020-11-12 19:11:30 -0500

    Clean up error handling
    GitOrigin-RevId: f7b8c054e5e81cf57bb14cbbeffe5482c938dea7

--- lib/c/die.c
+++ lib/c/die.c
@@ -1,6 +1,8 @@
 #include "base/tool.h"
 
+#include <errno.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,6 +26,25 @@ noreturn void die_errno_(const char *file, int line, int err, const char *fmt,
     va_end(ap);
     fputs(": ", stderr);
     fputs(strerror(err), stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+noreturn void die_read_(const char *file, int line, FILE *fp, const char *fmt,
+                        ...) {
+    int err = errno;
+    bool is_error = ferror(fp);
+    fprintf(stderr, "Error: %s:%d: ", file, line);
+    va_list(ap);
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fputs(": ", stderr);
+    if (is_error) {
+        fputs(strerror(err), stderr);
+    } else {
+        fputs("unexpected EOF", stderr);
+    }
     fputc('\n', stderr);
     exit(1);
 }
--- lib/c/tool.h
+++ lib/c/tool.h
@@ -4,6 +4,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdnoreturn.h>
 
 // Print an error message and exit.
@@ -17,6 +18,12 @@ noreturn void die_errno_(const char *file, int line, int err, const char *fmt,
                          ...) __attribute__((format(printf, 4, 5)));
 
 #define die_errno(err, ...) die_errno_(__FILE__, __LINE__, err, __VA_ARGS__)
+
+// Print an error message for a failed read. This will either be an IO error or
+// an unexpected EOF, depending on the state of ferror(fp) and feof(fp).
+noreturn void die_read_(const char *file, int line, FILE *fp, const char *fmt,
+                        ...) __attribute__((format(printf, 4, 5)));
+#define die_read(fp, ...) die_read_(__FILE__, __LINE__, fp, __VA_ARGS__)
 
 // Malloc, but exit the program on failure.
 void *xmalloc(size_t size) __attribute__((malloc));
